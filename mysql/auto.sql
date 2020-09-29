08892bcf8c532043a6ab8b2b483e54f46074c978
AlexK85 <saharr.k@mail.ru>
2020-09-29 11:06:35 +0300

    add connect

--- mysql/auto.sql
+++ mysql/auto.sql
@@ -1,9 +1,9 @@
-CREATE TABLE tbl (
+CREATE TABLE tbl_auto (
   id INT(11) NOT NULL AUTO_INCREMENT,
   name TINYTEXT NOT NULL,
   PRIMARY KEY (id)
 );
-INSERT INTO tbl VALUES (NULL, 'Процессоры');
-INSERT INTO tbl VALUES (NULL, 'Материнские платы');
-INSERT INTO tbl VALUES (NULL, 'Видеоадаптеры');
-SELECT * FROM tbl;
+INSERT INTO tbl_auto VALUES (NULL, 'Процессоры');
+INSERT INTO tbl_auto VALUES (NULL, 'Материнские платы');
+INSERT INTO tbl_auto VALUES (NULL, 'Видеоадаптеры');
+SELECT * FROM tbl_auto;
--- mysql/multi_insert.sql
+++ mysql/multi_insert.sql
@@ -1,9 +1,9 @@
-CREATE TABLE catalogs (
+CREATE TABLE catalogs_multi_insert (
   catalog_id INT(11) NOT NULL AUTO_INCREMENT,
   name TINYTEXT NOT NULL,
   PRIMARY KEY (catalog_id)
 );
-INSERT INTO catalogs VALUES (NULL, 'Процессоры'),
+INSERT INTO catalogs_multi_insert VALUES (NULL, 'Процессоры'),
 (NULL, 'Материнские платы'),
 (NULL, 'Видеоадаптеры'),
 (NULL, 'Жесткие диски'),
